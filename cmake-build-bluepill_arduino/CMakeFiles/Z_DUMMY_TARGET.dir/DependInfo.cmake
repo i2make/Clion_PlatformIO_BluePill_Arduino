52eccdca366c5baa66bbe70380d372e12f794ea6
i2make <48208304+i2make@users.noreply.github.com>
2021-01-04 23:42:41 +0900

    Add files via upload

---
+++ cmake-build-bluepill_arduino/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/kjh/CLionProjects/BluePill_Arduino/src/main.cpp" "C:/Users/kjh/CLionProjects/BluePill_Arduino/cmake-build-bluepill_arduino/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/avr"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/stm32"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/stm32/LL"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/stm32/usb"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/stm32/OpenAMP"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/stm32/usb/hid"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino/stm32/usb/cdc"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Drivers/STM32F4xx_HAL_Driver/Src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/STM32F1xx"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Middlewares/ST/STM32_USB_Device_Library/Core/Src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Middlewares/OpenAMP"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Middlewares/OpenAMP/open-amp/lib/include"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Middlewares/OpenAMP/libmetal/lib/include"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Middlewares/OpenAMP/virtual_driver"
+  "C:/Users/kjh/.platformio/packages/framework-cmsis/CMSIS/Core/Include"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/system/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc"
+  "C:/Users/kjh/.platformio/packages/framework-cmsis/CMSIS/DSP/Include"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/cores/arduino"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/variants/Generic_F103Cx"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/CMSIS_DSP/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/EEPROM/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/IWatchdog/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/Keyboard/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/Mouse/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/SPI/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/Servo/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/SoftwareSerial/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/SrcWrapper/src"
+  "C:/Users/kjh/.platformio/packages/framework-arduinoststm32/libraries/Wire/src"
+  "C:/Users/kjh/.platformio/packages/toolchain-gccarmnoneeabi/arm-none-eabi/include/c++/9.2.1"
+  "C:/Users/kjh/.platformio/packages/toolchain-gccarmnoneeabi/arm-none-eabi/include/c++/9.2.1/arm-none-eabi"
+  "C:/Users/kjh/.platformio/packages/toolchain-gccarmnoneeabi/lib/gcc/arm-none-eabi/9.2.1/include"
+  "C:/Users/kjh/.platformio/packages/toolchain-gccarmnoneeabi/lib/gcc/arm-none-eabi/9.2.1/include-fixed"
+  "C:/Users/kjh/.platformio/packages/toolchain-gccarmnoneeabi/arm-none-eabi/include"
+  "C:/Users/kjh/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
